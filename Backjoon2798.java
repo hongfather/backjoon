949f85e4c906ab7ac0f4b1275b6578eb1ce0863a
hongfather <86899433+hongfather@users.noreply.github.com>
2021-07-22 14:05:28 +0900

    2798번

---
+++ Backjoon2798.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Backjoon2798 {
+public static void main(String[] args) throws IOException {
+	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st= new StringTokenizer(bf.readLine());
+	int N = Integer.parseInt(st.nextToken());
+	int M = Integer.parseInt(st.nextToken());
+	int []arr = new int[N];
+	st= new StringTokenizer(bf.readLine());
+	for(int i=0;i<N;i++)
+		arr[i]=Integer.parseInt(st.nextToken());
+	int answer=1;
+	int sum;
+	for(int i=0;i<N-2;i++)
+		for(int j=i+1;j<N-1;j++)
+			for(int k=j+1;k<N;k++) {
+				sum=arr[i]+arr[j]+arr[k];
+				if(sum<=M && (M-sum) < (M-answer))
+					answer=sum;
+			}
+	System.out.println(answer);
+}
+}
