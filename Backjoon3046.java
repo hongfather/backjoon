d58de3e8005bc848e394ca6db9813ae16464b79b
hongfather <86899433+hongfather@users.noreply.github.com>
2021-07-22 14:35:26 +0900

    3046번

---
+++ Backjoon3046.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Backjoon3046 {
+public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int r1 = in.nextInt();
+	int s=in.nextInt();
+	System.out.println(2*s-r1);
+}
+}
